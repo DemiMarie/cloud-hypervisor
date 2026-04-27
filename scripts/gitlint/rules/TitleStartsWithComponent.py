96bd90faa7d4ef0db905e7666c456cdec56f5ac2
Demi Marie Obenour <demiobenour@gmail.com>
2026-04-27 00:13:29 -0400

    vhost-guest: add crate
    This adds a new crate for a virtio-vhost-user device.  This crate has no
    dependencies on the rest of Cloud Hypervisor, so it can be moved to
    rust-vmm in the future.
    
    Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>

--- scripts/gitlint/rules/TitleStartsWithComponent.py
+++ scripts/gitlint/rules/TitleStartsWithComponent.py
@@ -66,6 +66,7 @@ class TitleStartsWithComponent(LineRule):
             'vhost_user_block',
             'vhost_user_net',
             'virtio-devices',
+            'vhost-guest',
             'vm-allocator',
             'vm-device',
             'vmm',
